ef64b835118776035bd673e18f94c664b7092e85
soheon-lee <42701133+soheon-lee@users.noreply.github.com>
2018-09-30 17:25:03 +0900

    Add files via upload
    updated_0930

---
+++ main_head.js
@@ -0,0 +1,9 @@
+
+document.write('<h1><a href ="0_home.html"><img src="camera.jpg" height="64px">JAPANESE BY MY SELF!</a></h1>\
+<a id="youtube_yuhadayo" href   ="https://www.youtube.com/channel/UC_waGdcNiCWTv1GG9OvE23A" target ="_blank" title  ="유하다요"><img src="youtube_logo.jpg" height="30px"> with YUHADAYO</a>\
+<div id = "top_grid">\
+<span class = "top_grid"><h2 id="main1"><a href ="1-2_N4_2.html">WORDS</a></h2></span>\
+<span class = "top_grid"><h2 id="main2" ><a href ="2_grammar.html">GRAMMAR & TALKING</a></h2></span>\
+<span class = "top_grid"><h2 id="main3"><a href = "a.html">REVIEW</a></h2></span>\
+<span class = "top_grid"><h2 id="main4"><a href ="4_etc.html">et cetera</a></h2></span>\
+</div>')
