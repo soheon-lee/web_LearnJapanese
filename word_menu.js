3a13df412a1fb743467f0841647bc374ffb6eb7b
soheon-lee <42701133+soheon-lee@users.noreply.github.com>
2018-09-30 20:38:32 +0900

    Add files via upload

--- word_menu.js
+++ word_menu.js
@@ -2,13 +2,7 @@ document.write('\
 <div>\
   <ol>\
     <h3><li><a href ="1-1_N5.html" id="menu_N5">N5</a></li></h3>\
-    <h3><li><a href ="1-2_N4_2.html" id="menu_N4">N4</a></li></h3>\
+    <h3><li><a href ="1-2_N4.html" id="menu_N4">N4</a></li></h3>\
     <h3><li><a href ="1-3_N3.html" id="menu_N3">N3</a></li></h3>\
   </ol>\
-  <div class="page_number">\
-    page:<br>\
-    <a href="1-2_N4_1.html" id="pg_number_1">1</a>\
-    <a href="1-2_N4_2.html" id="pg_number_2">2</a>\
-    <a href="1-2_N4_3.html" id="pg_number_3">3</a>\
-  </div>\
 </div>')
